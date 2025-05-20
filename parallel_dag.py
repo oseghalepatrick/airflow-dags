fdc274971cfa1587bda23634c64c6f0c666af99f
Oseghale Ehizokhale Patrick <oseghale_patrick@yahoo.com>
2025-05-20 06:03:07 +0100

    Update parallel_dag.py

--- parallel_dag.py
+++ parallel_dag.py
@@ -1,8 +1,13 @@
 from airflow.decorators import dag, task
 from airflow.models.baseoperator import chain
 from time import sleep
+from airflow.utils.dates import days_ago
 
-@dag(schedule=None)
+@dag(
+    schedule=None,
+    start_date=days_ago(1),
+    catchup=False
+)
 def parallel_dag():
     @task
     def task_1():
